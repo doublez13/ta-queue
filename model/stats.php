c0356c516cc07c86a03efd1a4f8411ae15c900c3
Zane Zak <zz@eng.utah.edu>
2018-04-06 08:36:43 -0600

    Misc stats improvements

--- model/stats.php
+++ model/stats.php
@@ -13,7 +13,8 @@ function get_stud_log($stud_username){
   }
 
   $query = "SELECT (SELECT course_name FROM courses WHERE course_id=student_log.course_id) AS course_name, question, location, enter_tmstmp, help_tmstmp, exit_tmstmp, helped_by
-            FROM student_log WHERE username=? AND exit_tmstmp IS NOT NULL";
+            FROM student_log 
+            WHERE username=? AND exit_tmstmp !='0' AND help_tmstmp !='0'";
   $stmt  = mysqli_prepare($sql_conn, $query);
   if(!$stmt){
     mysqli_close($sql_conn);
@@ -51,7 +52,8 @@ function get_course_log($course_name){
   }
 
   $query = "SELECT username, question, location, enter_tmstmp, help_tmstmp, exit_tmstmp, helped_by
-            FROM student_log WHERE course_id=(SELECT course_id from courses where course_name=?) AND exit_tmstmp IS NOT NULL";
+            FROM student_log 
+            WHERE course_id=(SELECT course_id from courses where course_name=?) AND exit_tmstmp !='0' AND help_tmstmp !='0'";
   $stmt  = mysqli_prepare($sql_conn, $query);
   if(!$stmt){
     mysqli_close($sql_conn);
@@ -82,11 +84,6 @@ function get_course_log($course_name){
   return $result;
 }
 
-function get_daily_course_stats($course_name){
-
-
-}
-
 function get_stud_log_for_course($stud_username, $course_name){
   $sql_conn = mysqli_connect(SQL_SERVER, SQL_USER, SQL_PASSWD, DATABASE);
   if(!$sql_conn){
@@ -94,7 +91,8 @@ function get_stud_log_for_course($stud_username, $course_name){
   }
 
   $query = "SELECT question, location, enter_tmstmp, help_tmstmp, exit_tmstmp, helped_by
-            FROM student_log WHERE username=? AND course_id=(SELECT course_id from courses where course_name=?) AND exit_tmstmp IS NOT NULL";
+            FROM student_log 
+            WHERE username=? AND course_id=(SELECT course_id from courses where course_name=?) AND exit_tmstmp !='0' AND help_tmstmp !='0'";
   $stmt  = mysqli_prepare($sql_conn, $query);
   if(!$stmt){
     mysqli_close($sql_conn);
@@ -131,7 +129,8 @@ function get_ta_log_for_course($ta_username, $course_name){
   }
 
   $query = "SELECT question, location, enter_tmstmp, help_tmstmp, exit_tmstmp, helped_by
-            FROM student_log WHERE helped_by=? AND course_id=(SELECT course_id from courses where course_name=?) AND exit_tmstmp IS NOT NULL";
+            FROM student_log 
+            WHERE helped_by=? AND course_id=(SELECT course_id from courses where course_name=?) AND exit_tmstmp !='0' AND help_tmstmp !='0'";
   $stmt  = mysqli_prepare($sql_conn, $query);
   if(!$stmt){
     mysqli_close($sql_conn);
@@ -161,39 +160,18 @@ function get_ta_log_for_course($ta_username, $course_name){
   return $result;
 }
 
-function get_avg_wait_time($course_name){
+function get_course_stats($course_name){
   $sql_conn = mysqli_connect(SQL_SERVER, SQL_USER, SQL_PASSWD, DATABASE);
   if(!$sql_conn){
     return -1;
   }
 
-  $query = "SELECT AVG(TIME_TO_SEC(TIMEDIFF(help_tmstmp, enter_tmstmp))) AS wait_time 
-            FROM student_log WHERE help_tmstmp !='0' AND course_id=?";
-  $stmt  = mysqli_prepare($sql_conn, $query);
-  if(!$stmt){
-    mysqli_close($sql_conn);
-    return -1;
-  } 
-  mysqli_stmt_bind_param($stmt, "s", $course_name);
-  if(!mysqli_stmt_execute($stmt)){
-    mysqli_stmt_close($stmt);
-    mysqli_close($sql_conn);
-    return -1;
-  }
-  
-  mysqli_stmt_bind_result($stmt, $wait_time);
-  mysqli_stmt_fetch($stmt);
-  return $wait_time; 
-}
-
-function get_avg_help_time($course_name){
-  $sql_conn = mysqli_connect(SQL_SERVER, SQL_USER, SQL_PASSWD, DATABASE);
-  if(!$sql_conn){
-    return -1;
-  }
-
-  $query = "SELECT AVG(TIME_TO_SEC(TIMEDIFF(exit_tmstmp, help_tmstmp))) AS help_time 
-            FROM student_log WHERE exit_tmstmp !='0' AND course_id=?";
+  $query = "SELECT
+            AVG(TIME_TO_SEC(TIMEDIFF(help_tmstmp, enter_tmstmp)))    AS avg_wait_time,
+            AVG(TIME_TO_SEC(TIMEDIFF(exit_tmstmp, help_tmstmp)))     AS avg_help_time, 
+            STDDEV(TIME_TO_SEC(TIMEDIFF(exit_tmstmp, help_tmstmp)))  AS stddev_help_time
+            FROM student_log 
+            WHERE exit_tmstmp !='0' AND help_tmstmp !='0' AND course_id=?";
   $stmt  = mysqli_prepare($sql_conn, $query);
   if(!$stmt){
     mysqli_close($sql_conn);
@@ -211,7 +189,39 @@ function get_avg_help_time($course_name){
   return $help_time;
 }
 
-function get_daily_course_log($course_name){
+function get_course_usage_by_day($course_name){
+  $sql_conn = mysqli_connect(SQL_SERVER, SQL_USER, SQL_PASSWD, DATABASE);
+  if(!$sql_conn){
+    return -1;
+  }
 
+  $query = "SELECT
+            DATE(enter_tmstmp), COUNT(*)
+            FROM student_log 
+            WHERE exit_tmstmp !='0' AND help_tmstmp !='0' AND course_id=?
+            GROUP BY DATE(enter_tmstmp)";
+  $stmt  = mysqli_prepare($sql_conn, $query);
+  if(!$stmt){
+    mysqli_close($sql_conn);
+    return -1;
+  }
+  mysqli_stmt_bind_param($stmt, "s", $course_name);
+    if(!mysqli_stmt_execute($stmt)){
+    mysqli_stmt_close($stmt);
+    mysqli_close($sql_conn);
+    return -1;
+  }
+
+  mysqli_stmt_bind_result($stmt, $date, $count);
+  $result = [];
+  while (mysqli_stmt_fetch($stmt)){
+    $result[] = array("date"     => $date,
+                      "count"    => $count
+                     );
+  }
+
+  mysqli_stmt_close($stmt);
+  mysqli_close($sql_conn);
+  return $result;
 }
 ?>
