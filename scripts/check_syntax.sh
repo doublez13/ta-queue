9dd97b2e11b8fc5fb029f8484ef9e0ded4837936
Zane Zak <zz@eng.utah.edu>
2018-04-16 10:21:18 -0600

    Remove old file

--- scripts/check_syntax.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ROOT_DIR=/srv/queue/$USER
-
-php -l $ROOT_DIR/model/auth.php
-php -l $ROOT_DIR/model/config.php
-php -l $ROOT_DIR/model/courses.php
-php -l $ROOT_DIR/model/queue.php
-php -l $ROOT_DIR/model/tests.php
