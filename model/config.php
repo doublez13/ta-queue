542dd95020b1ab6c30138c118a2d4b73f314aa91
doublez13 <doublezane@gmail.com>
2018-04-18 13:55:05 -0600

    Update config.php

--- model/config.php
+++ model/config.php
@@ -10,4 +10,16 @@ define("SQL_SERVER",  "");
 define("SQL_USER",    "");
 define("SQL_PASSWD",  "");
 define("DATABASE",    "");
+
+/*
+ *This flag signifies that "TLS_REQCERT never" has been
+ *added to the ldap.conf file to disable cert checking.
+ *
+ *If this flag is not set, LDAP will fall back to 
+ *plaintext auth, which is NOT RECOMMENDED.
+ *
+ *This flag is meaningless on php >= 7.0.5, since we
+ *force TLS.
+ */
+define("TLS_REQCERT_DISABLED", true);
 ?>
