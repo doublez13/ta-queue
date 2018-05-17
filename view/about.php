f1d724aee57c6ca21e5761305cf8af061d3bde95
root <root@ta-queue.eng.utah.edu>
2018-05-17 11:43:28 -0600

    Spacing

--- view/about.php
+++ view/about.php
@@ -10,55 +10,56 @@
 <body>
   <?php include('./view/navbar.php'); ?>
 
-    <div class="jumbotron jumbotron-billboard" style="margin-top: -15px; opacity: 0.75;">
-        <div align="center" style="margin-top:  -40px; margin-bottom: -20px">
-            <h1 style="color: #404040; text-shadow: 2px 2px #000000;">Team Suzie Queue</h1>
-	        <img src="./resources/img/Untitled-1.png" alt="Suzie Queue Logo"style="height:70px;">
-        </div>
+  <div class="jumbotron jumbotron-billboard" style="margin-top: -15px; opacity: 0.75;">
+    <div align="center" style="margin-top:  -40px; margin-bottom: -20px">
+      <h1 style="color: #404040; text-shadow: 2px 2px #000000;">Team Suzie Queue</h1>
+	    <img src="./resources/img/Untitled-1.png" alt="Suzie Queue Logo"style="height:70px;">
     </div>
+  </div>
 
-    <div class="container">
-        <div class="col-sm-6">
-            <h1 style="color: #cc0000">Team Members</h1>
-                <h3 style="text-decoration: underline;">Blake Burton</h3> </br>
-		<p style="font-size: 17px">
-                    -API Design and Implementation </br>
-                    -UI / UX Polishing </br>
-                    -Quality Assurance </br>
-                    -Documentation </br>
-                    </br>
-                    Email: <a href="mailto:blakesterburt@gmail.com">blakesterburt@gmail.com</a>
-                </p>
+  <div class="container">
+    <div class="col-sm-6">
+      <h1 style="color: #cc0000">Team Members</h1>
+      <h3 style="text-decoration: underline;">Blake Burton</h3> </br>
+        <p style="font-size: 17px">
+          -API Design and Implementation </br>
+          -UI / UX Polishing </br>
+          -Quality Assurance </br>
+          -Documentation </br>
+          </br>
+          Email: <a href="mailto:blakesterburt@gmail.com">blakesterburt@gmail.com</a>
+        </p>
             
-                <h3 style="text-decoration: underline;">Ryan Welling</h3> </br>
-		<p style="font-size: 17px">
-                    -Front-end Design and Implementation</br>
-                    -UI / UX Polishing </br>
-                    -Reports</br>
-                    </br>
-                    Email: <a href="mailto:welling.ryan@gmail.com">welling.ryan@gmail.com</a>
-                </p>
-            
-                <h3 style="text-decoration: underline;">Zane Zakraisek</h3> </br>
-		<p style="font-size: 17px">
-                    -DevOps </br>
-                    -Back-end Design and Implementation</br>
-                    -Database Design and Implementation</br>
-                    -API Design and Implementation </br>
-                    -Security Validation </br>
-                    -Front-End Queue Implementation </br>
-                    -UI / UX Polishing </br>
-                    </br>
-                    Email: <a href="mailto:zz@eng.utah.edu">zz@eng.utah.edu</a>
-                </p>
-            </div><!--col-sm-12-->
-	    <div class="col-sm-6">
-	      <img src="./resources/img/SuzieQueueTeamPhoto.jpg" alt="Team Photo" style="width:600px; height:400px; margin-top:50px;">
-	      <p>
-		</br>
-		Team Members L-R: Blake Burton, Ryan Welling, Matt Damon, Zane Zakraisek
-	      </p>
-	    </div>
-        </div><!--container-->
+      <h3 style="text-decoration: underline;">Ryan Welling</h3> </br>
+		    <p style="font-size: 17px">
+          -Front-end Design and Implementation</br>
+          -UI / UX Polishing </br>
+          -Reports</br>
+          </br>
+          Email: <a href="mailto:welling.ryan@gmail.com">welling.ryan@gmail.com</a>
+        </p>
+          
+      <h3 style="text-decoration: underline;">Zane Zakraisek</h3> </br>
+        <p style="font-size: 17px">
+          -DevOps </br>
+          -Back-end Design and Implementation</br>
+          -Database Design and Implementation</br>
+          -API Design and Implementation </br>
+          -Security Validation </br>
+          -Front-End Queue Implementation </br>
+          -UI / UX Polishing </br>
+          </br>
+          Email: <a href="mailto:zz@eng.utah.edu">zz@eng.utah.edu</a>
+        </p>
+    </div><!--col-sm-6-->
+    
+    <div class="col-sm-6">
+	    <img src="./resources/img/SuzieQueueTeamPhoto.jpg" alt="Team Photo" style="width:600px; height:400px; margin-top:50px;">
+	    <p>
+      </br>
+		  Team Members L-R: Blake Burton, Ryan Welling, Matt Damon, Zane Zakraisek
+      </p>
+	  </div>
+  </div><!--container-->
 </body>
 </html>
