5a6f05c3a6ef88f6baa3a36c2417a764c32b3a59
Zane Zakraisek <doublezane@gmail.com>
2018-08-07 07:47:08 -0600

    Fix the demo script

--- scripts/demo_setup.php
+++ scripts/demo_setup.php
@@ -1,56 +1,56 @@
 <?php
 
-require_once '../auth.php';
-require_once '../courses.php';
-require_once '../queue.php';
+require_once '../model/auth.php';
+require_once '../model/courses.php';
+require_once '../model/queue.php';
 
 
-if (new_course("CS 1030: Foundations of CS", "CS", "1030", "The hard stuff 1!", "cs1030-queue")){
+if (new_course("CS 1030: Foundations of CS", "CS", "1030", "description", "rohith", NULL)){
   echo "Failed to create new course";
   die();
 }
 
-if (new_course("CS 1410: Introduction to Object-Oriented Programming", "CS", "1410", "The hard stuff 1!", "cs1410-queue")){
+if (new_course("CS 1410: Introduction to Object-Oriented Programming", "CS", "1410", "description", "rohith", NULL)){
   echo "Failed to create new course";
   die();
 }
 
-if (new_course("CS 2100: Descrete Scructures", "CS", "2100", "The hard stuff 1!", "cs2100-queue")){
+if (new_course("CS 2100: Descrete Scructures", "CS", "2100", "description", "rohith", NULL)){
   echo "Failed to create new course";
   die();
 }
 
-if (new_course("CS 2420: Introduction to Algorithms and Data Structures", "CS", "2420", "The hard stuff 1!", "cs2420-queue")){
+if (new_course("CS 2420: Introduction to Algorithms and Data Structures", "CS", "2420", "description", "rohith", NULL)){
   echo "Failed to create new course";
   die();
 }
 
-if (new_course("CS 3100: Models of Computation", "CS", "3100", "The hard stuff 1!", "cs3100-queue")){
+if (new_course("CS 3100: Models of Computation", "CS", "3100", "description", "rohith", NULL)){
   echo "Failed to create new course";
   die();
 }
 
-if (new_course("CS 3500: Software Practice 1", "CS", "3500", "The hard stuff 1!", "cs3500-queue")){
+if (new_course("CS 3500: Software Practice 1", "CS", "3500", "description", "rohith", NULL)){
   echo "Failed to create new course";
   die();
 }
 
-if (new_course("CS 3505: Software Practice 2", "CS", "3505", "The hard stuff 1!", "cs3505-queue")){
+if (new_course("CS 3505: Software Practice 2", "CS", "3505", "description", "rohith", NULL)){
   echo "Failed to create new course";
   die();
 }
 
-if (new_course("CS 3810: Computer Organization", "CS", "3810", "The hard stuff 1!", "cs3810-queue")){
+if (new_course("CS 3810: Computer Organization", "CS", "3810", "description", "rohith", NULL)){
   echo "Failed to create new course";
   die();
 }
 
-if (new_course("CS 4150: Algorithms", "CS", "4150", "The hard stuff 1!", "cs4150-queue")){
+if (new_course("CS 4150: Algorithms", "CS", "4150", "description", "rohith", NULL)){
   echo "Failed to create new course";
   die();
 }
 
-if (new_course("CS 4400: Computer Systems", "CS", "4400", "The hard stuff 1!", "cs4400-queue-test")){
+if (new_course("CS 4400: Computer Systems", "CS", "4400", "description", "rohith", NULL)){
   echo "Failed to create new course";
   die();
 }
