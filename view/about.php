c8631202a85d3d7b5c04fd881671f5bf556096f8
Zane Zakraisek <doublezane@gmail.com>
2018-08-12 17:35:38 -0600

    Added description to the About page

--- view/about.php
+++ view/about.php
@@ -10,7 +10,7 @@
   <?php include('./view/navbar.php'); ?>
 
   <div class="jumbotron jumbotron-billboard" style="margin-top: -15px; opacity: 0.75;">
-    <div style="margin-top:  -40px; margin-bottom: -20px; text-align: center;">
+    <div style="margin-top:  -40px; margin-bottom: -40px; text-align: center;">
       <h1 style="color: #404040; text-shadow: 2px 2px #000000;">Team Suzie Queue</h1>
       <img src="./resources/img/Untitled-1.png" alt="Suzie Queue Logo" style="height:70px;">
     </div>
@@ -50,14 +50,29 @@
           <br>
           Email: <a href="mailto:zz@eng.utah.edu">zz@eng.utah.edu</a>
         </p>
-    </div><!--col-sm-6-->
+    </div><!--col-sm-5-->
     
     <div class="col-sm-7">
-      <img src="./resources/img/SuzieQueueTeamPhoto.jpg" alt="Team Photo" style="width:600px; height:400px; margin-top:50px;">
+      <img src="./resources/img/SuzieQueueTeamPhoto.jpg" alt="Team Photo" style="width:450px; height:300px; margin-top:50px;">
       <p>
       <br>
       Team Members L-R: Blake Burton, Ryan Welling, Matt Damon, Zane Zakraisek
       </p>
+      
+      <p style="font-size: 17px">
+        This queue was built as our senior project for the 2017/2018 year. When we started this project, none of us had any web experience, but were vaguely familiar with some common web practices. We decided early on that we'd employ the common MVC web architecture for our project. One choice (mistake?) that separated us from the other web oriented teams was writing everything from scratch. Instead of using a common framwork like Ruby on Rails, we wrote the entire project from scratch, purely in PHP. Although this was a fantastic learning experience, a majority of time was spent writing boiler plate framework code. In fact, the entire backend was written using nothing more than the standard PHP libraries.
+      </p>
+      
+      <h3 style="text-decoration: underline;">Queue Components</h3>
+      <p style="font-size: 17px">
+        -<b>View</b>: All HTML, CSS, Javascript on the frontend<br> 
+        -<b>Controllers</b>: Parse all RESTfull API requests and call the appropriate model functions<br>
+        -<b>Model</b>: All logic and DB/LDAP interaction is done here.<br>
+        -<b>Router</b>: Every request comes through here. It either fetches the appropriate page, or sends the request on to the appropriate model
+      </p>
+
+      <p style="font-size: 17px">Please feel free to submit pull requests <a href="https://github.com/doublez13/suzie-queue">here</a>! </p>
+
     </div>
   </div><!--container-->
 </body>
