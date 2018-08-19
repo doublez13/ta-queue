a1e065db6449f5c92c6f1ad16ed8024b53803e3a
Zane Zakraisek <doublezane@gmail.com>
2018-08-19 09:29:42 -0600

    Couple touchups to the about page

--- view/about.php
+++ view/about.php
@@ -17,7 +17,7 @@
   </div>
 
   <div class="container">
-    <div class="col-sm-5">
+    <div class="col-sm-4">
       <h1 style="color: #cc0000">Team Members</h1>
       <h3 style="text-decoration: underline;">Blake Burton</h3> <br>
         <p style="font-size: 17px">
@@ -52,7 +52,7 @@
         </p>
     </div><!--col-sm-5-->
     
-    <div class="col-sm-7">
+    <div class="col-sm-8">
       <img src="./resources/img/SuzieQueueTeamPhoto.jpg" alt="Team Photo" style="width:450px; height:300px; margin-top:50px;">
       <p>
       <br>
@@ -60,15 +60,15 @@
       </p>
       
       <p style="font-size: 17px">
-        This queue was built as our senior project for the 2017/2018 year. When we started this project, none of us had any web experience, but were vaguely familiar with some common web practices. We decided early on that we'd employ the common MVC web architecture for our project. One choice (mistake?) that separated us from the other web oriented teams was writing everything from scratch. Instead of using a common framwork like Ruby on Rails, we wrote the entire project from scratch, purely in PHP. Although this was a fantastic learning experience, a majority of time was spent writing boiler plate framework code. In fact, the entire backend was written using nothing more than the standard PHP libraries.
+        This queue was built as our senior project for the 2017/2018 year. When we started this project, none of us had any web experience, but were vaguely familiar with some common web practices. We decided early on that we'd employ the common MVC web architecture for our project. One choice (mistake?) that separated us from the other web oriented teams was writing everything from scratch. Instead of using a common framwork like Ruby on Rails, we wrote the entire project from the ground up. In fact, the entire backend was written using nothing more than the standard PHP libraries. Although this was a fantastic learning experience, the majority of our time was spent writing boiler plate framework code.
       </p>
       
       <h3 style="text-decoration: underline;">Queue Components</h3>
       <p style="font-size: 17px">
-        -<b>View</b>: All HTML, CSS, Javascript on the frontend<br> 
-        -<b>Controllers</b>: Parse all RESTfull API requests and call the appropriate model functions<br>
+        -<b>View</b>: All HTML, CSS, Javascript on the frontend.<br> 
+        -<b>Controllers</b>: Parse all RESTfull API requests and call the appropriate model functions.<br>
         -<b>Model</b>: All logic and DB/LDAP interaction is done here.<br>
-        -<b>Router</b>: Every request comes through here. It either fetches the appropriate page, or sends the request on to the appropriate model
+        -<b>Router</b>: Every request comes through here. It either fetches the appropriate page, or sends the request on to the appropriate controller.
       </p>
 
       <p style="font-size: 17px">Please feel free to submit pull requests <a href="https://github.com/doublez13/suzie-queue">here</a>! </p>
