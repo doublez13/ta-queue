b8343834cc94aa02294b37a81e13b6ddea6bf024
Zane Zakraisek <doublezane@gmail.com>
2018-08-19 09:41:58 -0600

    Tidy up the help page

--- view/help.php
+++ view/help.php
@@ -19,13 +19,13 @@
     <div class="col-md-12">
       <h1 style="color: #cc0000">Account Creation</h1>
       <p style="font-size: 17px">
-        The queue currently makes use of College of Engineering CADE accounts. Any student with a CADE account can login using their credentials and their personal information will be pulled from the servers. Students without a CADE account can create one
+        The queue currently makes use of College of Engineering CADE accounts. Any student with a CADE account can login using their credentials and their personal information will be pulled from the upstream servers. Students without a CADE account can create one
         <a style="color: #e8002b" href="https://webhandin.eng.utah.edu/cade/create_account/index.php" target="_blank"> here.</a>
       </p>
 
       <h1 class="padding-top-20" style="color: #cc0000">Access Control</h1>
       <p style="font-size: 17px">
-        TA privileges are granted via course settings page on the queue. To be able to modify the settings for a course, a user must be a member of the admin group. This can be done by sending an email to opers@eng.utah.edu
+        TA privileges are granted via the course settings page. To be able to modify the settings for a course, a user must be a member of the admin group. This can be done by sending an email to opers@eng.utah.edu
       </p>
 
       <h1 class="padding-top-20" style="color: #cc0000">User Roles</h1>
@@ -40,17 +40,17 @@
 
       <h3>Teaching Assistant</h3>
       <p style="font-size: 17px">
-        Users may not enroll in a course if they belong to its TA group. If the user was already enrolled as a student in a course before being added to its TA group, they are automatically unenrolled as a student and assume their TA role.
+        Users may not enroll in a course as a student if they are already registered as a TA for that course. If the user was already enrolled as a student in a course before being added to its TA group, they are automatically unenrolled as a student and assume the TA role.
       </p>
 
       <h3>Administrator</h3>
       <p style="font-size: 17px">
-        If the user is an admin, they will be presented with a page that allows them to view and modify all courses. Additionally, they will see an admin dropdown menu in the navbar which allows them to create courses.
+        If the user is an Administrator, they will be presented with a page that allows them to view and modify all courses. Additionally, they will see an Admin dropdown menu in the navbar which allows them to create courses.
       </p>
 
       <h1 class="padding-top-20" style="color: #cc0000">Enrolling and Unenrolling in Courses</h1>
       <p style="font-size: 17px">
-        Upon login, users are routed to the Courses page. Here they can enroll in any course by simply clicking Enroll. If an access code is required, students must enter this before proceeding. This page is also where a student would unenroll from a course. In this case, the user can simply click Leave for the desired course.
+        Upon login, users are routed to the Courses page. Here they can enroll in any course by simply clicking Enroll. If an access code is required, students must enter that before proceeding. This page is also where a student would unenroll from a course. In this case, the user can simply click Leave for the desired course.
       </p>
 
       <h1 class="padding-top-20" style="color: #cc0000">Viewing Queues</h1>
@@ -74,15 +74,15 @@
       </p>
       <h3>Announcements</h3>
       <p style="font-size: 17px">
-        TAs are allowed to post announcements for their course. All announcements appear at the top of the screen, with newer announcements appearing towards the top. Announcements within the last fifteen minutes appear in green. TAs can delete an announcement by clicking the X next to it.
+        TAs are allowed to post announcements for their course. All announcements appear at the top of the screen, with newer announcements appearing towards the top. Announcements within the last hour appear in green. TAs can delete an announcement by clicking the X next to it.
       </p>
       <h3>Reordering</h3>
       <p style="font-size: 17px">
-        TAs may wish to reorder the queue. In this case, TAs can use the up and down arrow buttons next to a student to shift their position in the queue. Students also have the ability to move themselves down by clicking the down arrow on their row.
+        TAs may wish to reorder student in the queue. In this case, TAs can use the up and down arrow buttons next to a student to shift their position in the queue. Students also have the ability to move themselves down by clicking the down arrow on their row.
       </p>
       <h3>Time Limits</h3>
       <p style="font-size: 17px">
-        TAs may choose to impose time limits when helping students. In this case, TAs can set the time limit using the Time Limit Per Student feature. When a student's time has passed, the row corresponding to that student turns red.
+        TAs may choose to impose time limits when helping students. In this case, TAs can set the time limit using the Time Limit Per Student feature. When a student's time has passed, that student's row turns red.
       </p>
       <h3>Cool-down Timers</h3>
       <p style="font-size: 17px">
@@ -96,12 +96,12 @@
       </p>
       <h3>Access Codes</h3>
       <p style="font-size: 17px">
-        If desired, administrators may require an access code to enroll in a course. In this case, the course will appear yellow on the Courses page, and have a padlock icon next to it. Students will be required to enter the code to enroll. Access codes can include any ASCII character and be up to 16 characters long.
+        If desired, administrators may require an access code to enroll in a course. In this case, the course will appear yellow on the Courses page, and have a padlock icon next to it. Students will be required to enter a code to enroll. Access codes can include any ASCII character and be up to 16 characters long.
       </p>
 
       <h1 class="padding-top-20" style="color: #cc0000">Statistics</h1>
       <p style="font-size: 17px">
-        Nearly every queue operation is logged. When a student enters a queue, logged data includes the student's name, course, question, and timestamps for entering, getting helped, and exiting the queue, as well as the TA that helped them.
+        Nearly every queue operation is logged. When a student enters a queue, logged data includes the student's name, course, question, location, and timestamps for entering, getting helped, and exiting the queue, as well as the TA that helped them.
         The only metrics that are currently visible via the web interface are the number of students helped per day for a particular course.
         However, <a style="color: #e8002b" href="../swagger/index.html" target="_blank">public API endpoints</a> are available that give much more detailed information on a per course, per student, and per TA basis.
       </p>
