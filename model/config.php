964aed3e69868e0fa9eac966b5ade93cd6416fd4
doublez13 <doublezane@gmail.com>
2018-09-29 17:06:38 -0600

    Remove TLS flag in config now that TLS is required

--- model/config.php
+++ model/config.php
@@ -10,16 +10,4 @@ define("SQL_SERVER",  "");
 define("SQL_USER",    "");
 define("SQL_PASSWD",  "");
 define("DATABASE",    "");
-
-/*
- *This flag signifies that "TLS_REQCERT never" has been
- *added to the ldap.conf file to disable cert checking.
- *
- *If this flag is not set, LDAP will fall back to 
- *plaintext auth, which is NOT RECOMMENDED.
- *
- *This flag is meaningless on php >= 7.0.5, since we
- *force the flag to be set via php-ldap
- */
-define("TLS_REQCERT_DISABLED", true);
 ?>
