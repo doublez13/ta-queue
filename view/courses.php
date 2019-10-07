f289b712ab479313532a7085872ffa764ba92538
Zane Zakraisek <doublezane@gmail.com>
2019-10-06 19:27:55 -0600

    Better adapt course page layout for smaller screens

--- view/courses.php
+++ view/courses.php
@@ -27,7 +27,7 @@
     <div class="container">
 
       <div class="row">
-        <div class="col-sm-8 col-sm-offset-2">
+        <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
           <div id ="my_course_table" style="display: none">
             <div class="panel panel-primary">
               <!-- Default panel contents -->
@@ -49,7 +49,7 @@
       </div><!--row-->
 
       <div class="row">
-        <div class="col-sm-8 col-sm-offset-2">
+        <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
           <div id ="course_table" style="display: none">
             <div class="panel panel-primary">
               <!-- Default panel contents -->
