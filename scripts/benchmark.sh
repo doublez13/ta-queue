077789dbb5f3a8ec509027fc1107e81a54df5ba1
Zane Zakraisek <doublezane@gmail.com>
2019-11-17 18:03:21 -0700

    Add primitive benchmark script

---
+++ scripts/benchmark.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/php
+<?php
+require_once '../model/queue.php';
+
+#Warm up the cache
+get_queue(4, "student");
+
+$trials = 50000;
+$time_start = microtime(true);
+for ($x = 0; $x <= $trials; $x++) {
+  get_queue(4, "student");
+} 
+$time_end   = microtime(true);
+echo ($time_end - $time_start)/$trials;
+?>
