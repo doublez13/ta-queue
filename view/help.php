61950acaae163a884f76f72586f9c8653e6dd3d9
Zane Zakraisek <doublezane@gmail.com>
2019-12-24 13:06:31 -0700

    Update the help page with new features.

--- view/help.php
+++ view/help.php
@@ -28,18 +28,17 @@
     <div class="col-md-12">
       <h1 style="color: #cc0000">Account Creation</h1>
       <p style="font-size: 17px">
-        The queue currently makes use of College of Engineering CADE accounts. Any student with a CADE account can login using their credentials and their personal information will be pulled from the upstream servers. Students without a CADE account can create one
-        <a style="color: #e8002b" href="https://webhandin.eng.utah.edu/cade/create_account/index.php" target="_blank"> here.</a>
+        The TA queue currently makes use of University of Utah CIS accounts. Anyone with a CIS account can login and their information will be pulled from the upstream servers.
       </p>
 
       <h1 class="padding-top-20" style="color: #cc0000">Access Control</h1>
       <p style="font-size: 17px">
-        TA privileges are granted via the course settings page. To be able to modify the settings for a course, a user must be a member of the admin group. This can be done by sending an email to opers@eng.utah.edu
+        TA privileges are granted via the course settings page. To be able to modify the settings for a course, a user must be a member of the Admin group. Admin access may be granted by sending an email to opers@eng.utah.edu.
       </p>
 
       <h1 class="padding-top-20" style="color: #cc0000">User Roles</h1>
       <p style="font-size: 17px">
-        The queue currently has three major roles: Student, Teaching Assistant (TA), and Administrator.
+        The TA queue currently has three mutually exclusive roles on a per course basis: Student, Teaching Assistant (TA), and Instructor; and one global role: Administrator.
       </p>
 
       <h3>Student</h3>
@@ -49,12 +48,17 @@
 
       <h3>Teaching Assistant</h3>
       <p style="font-size: 17px">
-        Users may not enroll in a course as a student if they are already registered as a TA for that course. If the user was already enrolled as a student in a course before being added to its TA group, they are automatically unenrolled as a student and assume the TA role.
+        Teaching Assistants have full control over the queue for a particular course.
+      </p>
+
+      <h3>Instructor</h3>
+      <p style="font-size: 17px">
+        At the moment, instructors share the same permissions as Teaching Assistants. In the future, instructors should have permission to edit their courses, and access more detailed statistics.
       </p>
 
       <h3>Administrator</h3>
       <p style="font-size: 17px">
-        If the user is an Administrator, they will be presented with a page that allows them to view and modify all courses. Additionally, they will see an Admin dropdown menu in the navbar which allows them to create courses.
+        As an Administrator, users will be presented with a page that allows them to view and modify all courses, including adding and removing Students, Teaching Assistants, and Instructors. Additionally, Admins will see an Admin dropdown menu in the navbar which allows them to create courses and add or remove other Admins.
       </p>
 
       <h1 class="padding-top-20" style="color: #cc0000">Enrolling and Unenrolling in Courses</h1>
@@ -83,30 +87,39 @@
       </p>
       <h3>Announcements</h3>
       <p style="font-size: 17px">
-        TAs are allowed to post announcements for their course. All announcements appear at the top of the screen, with newer announcements appearing towards the top. Announcements within the last hour appear in green. TAs can delete an announcement by clicking the X next to it.
+        TAs and Instructors are allowed to post announcements for their course. All announcements appear at the top of the screen, with newer announcements appearing towards the top. Announcements within the last hour appear in green. TAs can delete an announcement by clicking the X next to it.
       </p>
       <h3>Reordering</h3>
       <p style="font-size: 17px">
-        TAs may wish to reorder student in the queue. In this case, TAs can use the up and down arrow buttons next to a student to shift their position in the queue. Students also have the ability to move themselves down by clicking the down arrow on their row.
+        TAs may wish to reorder students in the queue. In this case, TAs can use the up and down arrow buttons next to a student to shift their position in the queue. Students also have the ability to move themselves down by clicking the down arrow button on their row.
       </p>
       <h3>Time Limits</h3>
       <p style="font-size: 17px">
-        TAs may choose to impose time limits when helping students. In this case, TAs can set the time limit using the Time Limit Per Student feature. When a student's time has passed, that student's row turns red.
+        TAs may choose to impose time limits when helping students. In this case, TAs can set the time limit using the Time Limit Per Student feature. When a student's time has passed, that student's row turns red. A time limit of zero (default) indicates no time limit.
       </p>
       <h3>Cool-down Timers</h3>
       <p style="font-size: 17px">
-        TAs may wish to impose cool-down timers for students. If a student gets helped, they are not allowed to reenter the queue until the specified amount of minutes has passed. TAs can set the cool-down time using the Cool-down feature.
+        TAs may wish to impose cool-down timers for students. If a student gets helped, they are not allowed to reenter the queue until the specified amount of minutes has passed. TAs can set the cool-down time using the Cool-down feature. A time limit of zero (default) indicates no time limit.
       </p>
 
-      <h1 class="padding-top-20" style="color: #cc0000">Creating a New Course</h1>
-      <h3>Assigning and Removing TAs</h3>
+      <h1 class="padding-top-20" style="color: #cc0000">Course Settings</h1>
+      <h3>Assigning and Removing Roles</h3>
       <p style="font-size: 17px">
-        TAs for a course are assigned on the course creation page. The list may also be modified at any time by clicking the gear icon next to a course. 
+        Students, TAs, and Instructors for a course can be assigned on the course creation page. These roles may be modified any time by clicking the gear icon next to a course. 
       </p>
-      <h3>Access Codes</h3>
+      <h3>Access Code</h3>
       <p style="font-size: 17px">
         If desired, administrators may require an access code to enroll in a course. In this case, the course will appear yellow on the Courses page, and have a padlock icon next to it. Students will be required to enter a code to enroll. Access codes can include any ASCII character and be up to 16 characters long.
       </p>
+      <h3>Generic</h3>
+      <p style="font-size: 17px">
+        During course creation, a course can be marked as Generic to signify that it is not associated with one particular course. On the queue page, students are required to enter the course they need help with along with their question.
+      </p>
+      <h3>Enabled</h3>
+      <p style="font-size: 17px">
+        If a course is marked as Enabled, it will appear on the courses list for non Admins. If the course is not marked as Enabled, only Admins can see it on the courses page.
+      </p>
+
 
       <h1 class="padding-top-20" style="color: #cc0000">Statistics</h1>
       <p style="font-size: 17px">
