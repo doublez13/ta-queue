9ebc6ccf36b16d3d12eeabbd460f4fe686018bf5
Zane Zakraisek <doublezane@gmail.com>
2022-02-03 13:04:43 -0700

    Fixed rendering on iOS.
    Reported by @Mgomez-01

--- src/notification.js
+++ src/notification.js
@@ -1,5 +1,9 @@
 //Based on https://developer.mozilla.org/en-US/docs/Web/API/notification
 function notify_grant(){
+  if (!('Notification' in window)) {
+    console.log("This browser does not support notifications.");
+    return;
+  }
   // Otherwise, we need to ask the user for permission
   if (Notification.permission !== "granted" && Notification.permission !== "denied") {
     Notification.requestPermission().then(function (permission) {
@@ -12,6 +16,10 @@ function notify_grant(){
 }
 
 function notify(notify_text) {
+  if (!('Notification' in window)) {
+    console.log("This browser does not support notifications.");
+    return;
+  }
   // Let's check whether notification permissions have already been granted
   if (Notification.permission === "granted") {
     // If it's okay let's create a notification
